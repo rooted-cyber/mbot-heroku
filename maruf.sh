9f4f7b5a35e67ff6c6d0f8fc76c86faa10e1af32
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-08-02 13:55:41 +0530

    Update maruf.sh

--- maruf.sh
+++ maruf.sh
@@ -1,5 +1,5 @@
 apt install git -y
-git clone https://github.com/rootedcyber93/helios-mirror
+git clone https://github.com/maruf-android/helios-mirror
 cd helios*
 pip3 install --no-cache-dir -r requirements.txt
 bash start.sh
